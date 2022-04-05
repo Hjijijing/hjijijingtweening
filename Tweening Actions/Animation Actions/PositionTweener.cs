6296f2050104222f71eb66cfb1ebf7bb5f9d5741
Rasmus Hansen <rashansen69@gmail.com>
2022-04-05 20:53:40 +0200

    Anchored Position tweening for rect transforms

--- Tweening Actions/Animation Actions/PositionTweener.cs
+++ Tweening Actions/Animation Actions/PositionTweener.cs
@@ -27,6 +27,30 @@ namespace hjijijing.Tweening
         }
     }
 
+
+    public class AnchoredPositionTweener : AnimationTweeningAction<Vector3>
+    {
+
+        public AnchoredPositionTweener(Action<ITweener> onDone, MonoBehaviour mono, GameObject gameObject, Vector3 endValue, float duration, float startDelay = 0f, float endDelay = 0f) : base(onDone, mono, gameObject, endValue, duration, startDelay, endDelay) { }
+
+        public override ITweeningAction getReverse(Action<ITweener> onDone)
+        {
+            return new AnchoredPositionTweener(onDone, mono, gameObject, startValue, duration, startDelay, endDelay);
+        }
+
+        public override void modifyGameObject(float time)
+        {
+            if (!(gameObject.transform is RectTransform)) return;
+            ((RectTransform)gameObject.transform).anchoredPosition = Vector3.LerpUnclamped(startValue, endValue, time);
+        }
+
+        public override void findStartValue()
+        {
+            if (!(gameObject.transform is RectTransform)) return;
+            startValue = ((RectTransform)gameObject.transform).anchoredPosition;
+        }
+    }
+
 }
 
 
